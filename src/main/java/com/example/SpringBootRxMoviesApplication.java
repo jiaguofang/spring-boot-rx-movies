05f609d0d2f5ca52a871eca495b999dc6b3ec3fa
Jiaguo Fang <jiaguo.fang@capitalone.com>
2017-01-08 00:20:01 -0800

    Support MDC

--- src/main/java/com/example/SpringBootRxMoviesApplication.java
+++ src/main/java/com/example/SpringBootRxMoviesApplication.java
@@ -1,8 +1,10 @@
 package com.example;
 
+import com.example.controller.MdcPropagatingOnScheduleAction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import rx.plugins.RxJavaHooks;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -16,6 +18,8 @@ public class SpringBootRxMoviesApplication {
     }
 
     public static void main(String[] args) {
+        // Set MDC
+        RxJavaHooks.setOnScheduleAction(new MdcPropagatingOnScheduleAction());
         SpringApplication.run(SpringBootRxMoviesApplication.class, args);
     }
 }
---
+++ src/main/java/com/example/controller/MdcPropagatingAction.java
@@ -0,0 +1,62 @@
+package com.example.controller;
+
+/*
+ * Copyright 2016 bmcstdio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import rx.functions.Action0;
+
+import java.util.Map;
+
+/**
+ * Decorates an {@link Action0} so that it executes with the current {@link MDC} as its context.
+ */
+public final class MdcPropagatingAction implements Action0 {
+    private final Action0 action0;
+    private final Map<String, String> context;
+    private static Logger logger = LoggerFactory.getLogger(MdcPropagatingAction.class);
+
+    /**
+     * Decorates an {@link Action0} so that it executes with the current {@link MDC} as its context.
+     *
+     * @param action0 the {@link Action0} to decorate.
+     */
+    public MdcPropagatingAction(final Action0 action0) {
+        logger.info("MdcPropagatingAction::MdcPropagatingAction " + action0);
+        this.action0 = action0;
+        this.context = MDC.getCopyOfContextMap();
+    }
+
+    @Override
+    public void call() {
+        final Map<String, String> originalMdc = MDC.getCopyOfContextMap();
+        logger.info("MdcPropagatingAction::call " + action0);
+        if (context != null) {
+            MDC.setContextMap(context);
+        }
+        try {
+            this.action0.call();
+        } finally {
+            if (originalMdc != null) {
+                MDC.setContextMap(originalMdc);
+            }
+        }
+    }
+}
+
